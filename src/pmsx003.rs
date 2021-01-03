58f12fced2fad5ae95a789f9e977bf233d062653
Andelf <andelf@gmail.com>
2021-01-04 00:16:49 +0800

    refactor(PMSx003): refactor value parsing

--- src/pmsx003.rs
+++ src/pmsx003.rs
@@ -11,7 +11,6 @@ const PREFIX2: u8 = 0x4d;
 
 pub struct PMSx003<S> {
     serial: S,
-    read_buf: [u8; 30],
 }
 
 pub struct Measurements {
@@ -41,65 +40,60 @@ where
     S: serial::Read<u8> + serial::Write<u8>,
 {
     pub fn new(serial: S) -> Self {
-        Self {
-            serial,
-            read_buf: [0u8; 30],
-        }
+        Self { serial }
     }
 
     pub fn read_measurements(&mut self) -> Result<Measurements, ()> {
+        // NOTE: 0x00_1c = 28, number of remaining bytes
+        const PREFIX: [u8; 4] = [PREFIX1, PREFIX2, 0x00, 0x1c];
+
+        let mut i = 0;
+        let mut buf = [0u8; 26];
+        let mut checksum: u16 = 0;
+
         loop {
-            self.read_until(PREFIX1);
-            match self.read_byte() {
-                Ok(b) if b == PREFIX2 => break,
-                _ => continue,
+            let c = self.read_byte()?;
+            match i {
+                0..=3 if c == PREFIX[i] => {
+                    checksum += c as u16;
+                    i += 1;
+                }
+                4..=29 => {
+                    checksum += c as u16;
+                    buf[i - 4] = c;
+                    i += 1;
+                }
+                30 if c == (checksum >> 8) as u8 => {
+                    i += 1;
+                }
+                31 if c == (checksum & 0xff) as u8 => {
+                    break;
+                }
+                _ => {
+                    i = 0;
+                    checksum = 0;
+                }
             }
-        }
-        let mut sum: u16 = (PREFIX1 + PREFIX2) as u16;
-        // remain 30 bytes
-        for i in 0..30 {
-            let b = self.read_byte().unwrap();
-            self.read_buf[i] = b;
-            if i < 28 {
-                sum += b as u16;
-            }
-        }
-        let buf = &self.read_buf[..];
-        if buf[0] != 0x00 || buf[1] != 0x1c {
-            return Err(()); // length error, might restart
-        }
-        if buf[28] != (sum >> 8) as u8 || buf[29] != (sum & 0xff) as u8 {
-            return Err(()); // checksum error
         }
 
         Ok(Measurements {
-            cf1_pm1_0: u16::from_be_bytes(buf[2..4].try_into().unwrap()),
-            cf1_pm2_5: u16::from_be_bytes(buf[4..6].try_into().unwrap()),
-            cf1_pm10: u16::from_be_bytes(buf[6..8].try_into().unwrap()),
-            pm1_0: u16::from_be_bytes(buf[8..10].try_into().unwrap()),
-            pm2_5: u16::from_be_bytes(buf[10..12].try_into().unwrap()),
-            pm10: u16::from_be_bytes(buf[12..14].try_into().unwrap()),
-            db0_3_um: u16::from_be_bytes(buf[14..16].try_into().unwrap()),
-            db0_5_um: u16::from_be_bytes(buf[16..18].try_into().unwrap()),
-            db1_0_um: u16::from_be_bytes(buf[18..20].try_into().unwrap()),
-            db2_5_um: u16::from_be_bytes(buf[20..22].try_into().unwrap()),
-            db5_0_um: u16::from_be_bytes(buf[22..24].try_into().unwrap()),
-            db10_um: u16::from_be_bytes(buf[24..26].try_into().unwrap()),
+            cf1_pm1_0: u16::from_be_bytes(buf[0..2].try_into().unwrap()),
+            cf1_pm2_5: u16::from_be_bytes(buf[2..4].try_into().unwrap()),
+            cf1_pm10: u16::from_be_bytes(buf[4..6].try_into().unwrap()),
+            pm1_0: u16::from_be_bytes(buf[6..8].try_into().unwrap()),
+            pm2_5: u16::from_be_bytes(buf[8..10].try_into().unwrap()),
+            pm10: u16::from_be_bytes(buf[10..12].try_into().unwrap()),
+            db0_3_um: u16::from_be_bytes(buf[12..14].try_into().unwrap()),
+            db0_5_um: u16::from_be_bytes(buf[14..16].try_into().unwrap()),
+            db1_0_um: u16::from_be_bytes(buf[16..18].try_into().unwrap()),
+            db2_5_um: u16::from_be_bytes(buf[18..20].try_into().unwrap()),
+            db5_0_um: u16::from_be_bytes(buf[20..22].try_into().unwrap()),
+            db10_um: u16::from_be_bytes(buf[22..24].try_into().unwrap()),
         })
     }
 
     pub fn release(self) -> S {
         self.serial
-    }
-
-    fn read_until(&mut self, byte: u8) {
-        loop {
-            if let Ok(b) = self.serial.read() {
-                if b == byte {
-                    return;
-                }
-            }
-        }
     }
 
     fn read_byte(&mut self) -> Result<u8, ()> {
